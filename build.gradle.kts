6ec6a5361a714d2e736cc756f3aa54c163a61031
NathanFallet <contact@nathanfallet.me>
2026-03-19 05:40:27 +0100

    v1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
 
 allprojects {
     group = "me.nathanfallet.pokemonmaprandomizer"
-    version = "1.0.1"
-    project.ext.set("url", "https://github.com/nathanfallet/pokemon-map-randomizer")
+    version = "1.0.2"
+    project.ext.set("url", "https://github.com/kotlinds/pokemon-map-randomizer")
     project.ext.set("license.name", "Apache 2.0")
     project.ext.set("license.url", "https://www.apache.org/licenses/LICENSE-2.0.txt")
     project.ext.set("developer.id", "nathanfallet")
     project.ext.set("developer.name", "Nathan Fallet")
     project.ext.set("developer.email", "contact@nathanfallet.me")
     project.ext.set("developer.url", "https://www.nathanfallet.me")
-    project.ext.set("scm.url", "https://github.com/nathanfallet/pokemon-map-randomizer.git")
+    project.ext.set("scm.url", "https://github.com/kotlinds/pokemon-map-randomizer.git")
 }
 
 dependencies {
